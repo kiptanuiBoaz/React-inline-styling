093dcc44f52230e0cc62fb0ed7bc3ef9dd696d64
kiptanui boaz <84452739+kiptanuiBoaz@users.noreply.github.com>
2022-05-21 08:19:42 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const CustomStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+CustomStyle.color = "green";
+
+ReactDOM.render(
+  <h1 style={CustomStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
